b0e728e8161a3304bb93e691ddc3b45e110755e5
Meshchaninov-A <enkooo14@gmail.com>
2020-08-14 15:28:20 +0300

    Коммит скелета проекта

---
+++ PhoneBook/src/test/java/PhoneBookTest.java
@@ -0,0 +1,30 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PhoneBookTest {
+
+    @Test
+    void checkPhoneNumber() {
+    }
+
+    @Test
+    void checkName() {
+    }
+
+    @Test
+    void formatName() {
+    }
+
+    @Test
+    void formatPhoneNumber() {
+    }
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void list() {
+    }
+}
